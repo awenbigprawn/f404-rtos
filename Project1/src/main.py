c49e0a5e64fd966e695c2beb578e02220ed63b43
Exora02 <antoine.leyvraz@gmail.com>
2024-10-29 15:34:51 +0100

    added parsing of arguments and of taskset file

--- Project1/src/main.py
+++ Project1/src/main.py
@@ -11,22 +11,28 @@ def parseArgs():
 
 
 if __name__ == "__main__":
-    scheduling_algorithm, taskset_file = parseArgs()
+    args = parseArgs()
+    scheduling_algorithm = args.algorithm
+    taskset_file = args.file
 
     task_set = datatypes.TaskSet(tasks=[])
     try:
         with open(taskset_file, 'r') as file:
-            for line in file:
-                data = line.split(",").strip()
+            for i, line in enumerate(file):
+                data = line.split(",")
                 O, C, D, T = data
-                new_task = datatypes.Task()
-                new_task.offset = int(O)
-                new_task.computation_time = int(C)
-                new_task.deadline = int(D)
-                new_task.period = int(T)
+                new_task = datatypes.Task(
+                    task_id=i,
+                    name="Task_"+str(i),
+                    offset=int(O),
+                    computation_time=int(C),
+                    deadline=int(D),
+                    period=int(T),
+                    priority=0
+                )
                 task_set.tasks.append(new_task)
 
-                
+
     except FileNotFoundError:
         print("File not found, please check the provided path")
     
