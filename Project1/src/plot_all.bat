0faa3e5275b17f7b4bd609a5dd524241e9036800
Exora02 <antoine.lagnaz@hotmail.fr>
2024-11-08 14:33:20 +0100

    added bash equivalent for plot all

--- Project1/src/plot_all.bat
+++ Project1/src/plot_all.bat
@@ -3,3 +3,9 @@ for /D %%d in (./tasksets/80-percent/*) do (
         python ./src/plot.py %%a ./tasksets/80-percent/%%d
     )
 )
+
+for /D %%d in (./tasksets/10-tasks/*) do (
+    for %%a in (dm edf rr) do (
+        python ./src/plot.py %%a ./tasksets/10-tasks/%%d
+    )
+)
